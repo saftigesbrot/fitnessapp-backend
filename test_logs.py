538d56ba0d6e68a2aa16a8ee9a58049da3a3f435
Marius Schulze <schulzemarius30@gmail.com>
2026-03-01 21:49:06 +0100

    Fix Image Error

---
+++ test_logs.py
@@ -0,0 +1,1 @@
+# Checking django logs for the 400 Bad Request to see what the serializer complained about
---
+++ test_upload.py
@@ -0,0 +1,4 @@
+import requests
+
+# Just to see if django is throwing a 400 with a specific error message
+# Wait, actually we have access to the backend logs.
